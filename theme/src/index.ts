537a5934b5e4cd3437809a17a7694c155bd01b69
Colby M. White <3979735+undefined@users.noreply.github.com>
2022-12-10 19:13:03 +0000

    First commit

---
+++ theme/src/index.ts
@@ -0,0 +1,18 @@
+import { typography } from '~/typography';
+import { colors } from '~/colors';
+import { border } from '~/border';
+import { opacity } from '~/opacity';
+import { shadows } from '~/shadows';
+import { spacing } from '~/spacing';
+import { zIndex } from '~/z-index';
+
+export const theme = {
+  extend: {
+    ...typography,
+    ...border,
+    ...opacity,
+    ...shadows,
+    ...spacing,
+    ...zIndex,
+  },
+};
